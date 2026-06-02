846633657a947e746430689b87126195e96577f8
Ignacio_Garcia <iggarsaudev@gmail.com>
2026-06-02 13:31:57 +0200

    feat: añade sistema de notificaciones global toast con signals

--- safebank-frontend/src/app/app.routes.ts
+++ safebank-frontend/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // redirección por defecto al login
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+  // aplicamos lazy loading (carga perezosa) para optimizar el rendimiento
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/auth/components/login/login.component').then(
+        (m) => m.LoginComponent,
+      ),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./features/auth/components/register/register.component').then(
+        (m) => m.RegisterComponent,
+      ),
+  },
+];
